c41839ea3162d904995672c5757f99e6b94e8d27
mohamedresk23 <mohammedresk4@gmail.com>
2024-05-15 19:36:14 +0300

    swiper broduct done

---
+++ js/swiper.js
@@ -0,0 +1,51 @@
+// swiper sidebar
+var swiper = new Swiper(".slider-swp", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullests:true,
+      clickable: true,
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    loop: true,
+  });
+
+// swiper sale
+var swiper = new Swiper(".sale-sec", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullests:true,
+      clickable: true,
+    },
+    slidesPerView: 1,
+    spaceBetween: 30,
+    breakpoints: {
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 40,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 30,
+        },
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        420: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+  });
