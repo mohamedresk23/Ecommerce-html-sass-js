c4d423a007b8ebf3d6f5f22230e814616bb4d09c
mohamedresk23 <mohammedresk4@gmail.com>
2024-05-21 17:55:49 +0300

    add to caert

--- js/items_home.js
+++ js/items_home.js
@@ -3,13 +3,14 @@ let swiper_products_sale = document.getElementById("swiper-products-sale");
 fetch("js/items.json")
 .then(res => res.json())
 .then(data => {
+    all_products_json = data;
     data.forEach(product => {
         if (product.old_price) {
             swiper_products_sale.innerHTML += `
         <div class="product swiper-slide">
         <span class="sale-pers">${Math.floor(((product.old_price - product.price)/ product.old_price)*100)}%</span>
         <div class="icons">
-          <i class="fa-solid fa-cart-plus"></i>
+          <i class="fa-solid fa-cart-plus "  onclick="addToCart(${product.id},this)" ></i>
           <i class="fa-solid fa-heart"></i>
           <i class="fa-solid fa-share"></i>
         </div>
--- js/main.js
+++ js/main.js
@@ -10,3 +10,32 @@ const closeCar= () => {
 }
 icon_cart.addEventListener("click",openCart);
 close_icon.addEventListener("click",closeCar);
+
+
+let all_products_json;
+
+let items_in_cart = document.querySelector(".items-in-cart");
+let product_cart=[];
+const addToCart = (id,btn) => {
+    product_cart.push(all_products_json[id]);
+    btn.classList.add("active");
+    getCartItems();
+    console.log(product_cart);
+}
+
+const getCartItems = () => {
+    let items_cart ="";
+    for (let i = 0; i < product_cart.length; i++) {
+        items_cart += `
+        <div class="item-cart">
+        <img src="${product_cart[i].img}" alt="product">
+        <div class="containt">
+          <h4>"${product_cart[i].name}</h4>
+          <div class="price">"${product_cart[i].price}$</div>
+        </div>
+        <button class="delete"><i class="fa-regular fa-trash-can"></i></button>
+      </div>`
+        
+    }
+    items_in_cart.innerHTML = items_cart;
+}
